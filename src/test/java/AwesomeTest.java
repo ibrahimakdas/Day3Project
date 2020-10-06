fd927e95d091ccda61423f8c68c7131fec1a2f76
ibrahimakdas <iaibrahimakdas@gmail.com>
2020-10-06 22:44:55 +0300

    awesome hello

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -10,4 +10,11 @@ public class AwesomeTest {
 
     // dont be so clever
 
+    //task final
+    // 1. goto githup, fork my repository in to yours
+    //2. new clone the forked repo from your account into intellij
+    //3. add a folder under src/test /java with your github username
+    //4. add a class under this username
+    //5.push it back to your own github
+    //6.
 }

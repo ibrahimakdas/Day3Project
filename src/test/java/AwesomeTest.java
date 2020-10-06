35d5da5b078a8b3a4d9dd7765b0fe41228c21ea0
ibrahimakdas <iaibrahimakdas@gmail.com>
2020-10-06 21:58:47 +0300

    added more awesome stuffs

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -3,4 +3,11 @@ public class AwesomeTest {
     // red
     //green
     //no color
+
+    // ı just change smt ı
+    // I did it myself
+    // Now I made smt to you
+
+    // dont be so clever
+
 }
